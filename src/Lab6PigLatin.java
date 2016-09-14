cd4ef9635135f5a9702108ceaf09bf9594f66912
Kimberli Smith <kimsmith.it@gmail.com>
2016-09-14 01:16:43 -0400

    added do while loop

--- src/Lab6PigLatin.java
+++ src/Lab6PigLatin.java
@@ -23,8 +23,11 @@ public class Lab6PigLatin {
 		char firstLetter;
 		String text; //whatever user inputs
 		String vow = "a,e,i,o,u";
+		String choice = "y";
 		
 		System.out.println("Pig Latin Translator");
+		
+do	{ //initiate do-while loop		
 		System.out.println("Enter text to translate: ");
 		Scanner k = new Scanner(System.in);
 		text = k.nextLine();
@@ -33,8 +36,11 @@ public class Lab6PigLatin {
 		firstLetter = text.charAt(0);//identifies letter in first place
 		vowelFirst = vow.indexOf(firstLetter);//determines if first letter is a vowel
 		
-		boolean firstLetterVowel = (vowelFirst >= 0);
-		
+		boolean firstLetterVowel = (vowelFirst >= 0);//moves characters
+		System.out.println("Continue? y/n");
+		k.nextLine();
+		choice = k.nextLine();
+	}while(choice.equals("y")); //ends do-while loop
 
 	}//end main method
 }//end public class Lab6PigLatin
