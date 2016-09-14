36eaa7d844501383b60de2f2d8ed26a3a9b4b48b
Kimberli Smith <kimsmith.it@gmail.com>
2016-09-14 16:12:12 -0400

    got this to work thanks to kevin, tyler, and jeseekia

--- src/Lab6PigLatin.java
+++ src/Lab6PigLatin.java
@@ -5,6 +5,7 @@
 
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 /**
  * @author Kimberli
@@ -19,35 +20,47 @@ public class Lab6PigLatin {
 	 */
 	public static void main(String[] args) {
 		
-		int vowelFirst;
+		int vowelFirst = 0;
+		int posOfVowel = 0;
 		char firstLetter;
-		String text; //whatever user inputs
-		String ext; // user input without first letter
-		String vow = "a,e,i,o,u";
+		String text; //whatever user inputs.
+	    String front; //beginning of word
+		String back; // user input without first letter
+		
 		String choice = "y";
 		
 		System.out.println("Pig Latin Translator");
 		
-	do	{ //initiate do-while loop		
+	
+		do	{ //initiate do-while loop	
+		
 		System.out.println("Enter text to translate: ");
 		Scanner k = new Scanner(System.in);
 		text = k.nextLine();
-		text = text.toLowerCase();/* method named to make all letters lower case */
+				
+		text = text.toLowerCase();/* method  to make all letters lower case */
 		
 		firstLetter = text.charAt(0);//identifies letter in first place
-		vowelFirst = vow.indexOf(firstLetter);//determines if first letter is a vowel
+	
 		
-		boolean firstLetterVowel = (vowelFirst >= 0);//moves characters
-		ext = text.substring(1); //captures word without first letter - get it?
+		boolean firstLetterVowel = (text.charAt(0)== 'a' || text.charAt(0) == 'e' || text.charAt(0)== 'i' || text.charAt(0) == 'o' || text.charAt(0) == 'u');
 		
 		if (firstLetterVowel) {
-			System.out.println(text + "way");
+			System.out.println("Translation: " +text + "way");
 		}// end if
 		else {
-			System.out.println(ext + "ay");
+			for(int i = 0; i < text.length();  i++){
+				if(text.charAt(i)== 'a' || text.charAt(i) == 'e' || text.charAt(i)== 'i' || text.charAt(i) == 'o' || text.charAt(i) == 'u'){
+					posOfVowel = i;
+				break;
+				}
+			}
+			front = text.substring(0,posOfVowel);
+			back = text.substring(posOfVowel); //captures word without first letter
+
+			System.out.println("Translation: " +back + front+ "ay");
 		}//end else
 		System.out.println("Continue? y/n");
-		k.nextLine();
 		choice = k.nextLine();
 	}while(choice.equals("y")); //ends do-while loop
 
